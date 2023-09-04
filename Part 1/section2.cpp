35ea069046df0388a3444bd4223884f8aa504840
Tristan Long <longtr@oregonstate.edu>
2023-09-04 14:36:32 -0400

    Struct practice 2

--- Part 1/section2.cpp
+++ Part 1/section2.cpp
@@ -6,12 +6,17 @@ using namespace std;
 struct Rectangle{
     int length;
     int breadth;
+    char x;
 };
 
 int main(){
     struct Rectangle r1={10,5};
 
-    printf("%d", sizeof(r1));
+    r1.length=15;
+    r1.breadth=7;
+
+    cout<<r1.length<<endl;
+    cout<<r1.breadth<<endl;
 
     return 0;
 }
