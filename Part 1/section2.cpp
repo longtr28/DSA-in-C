02ed7e9ef9ef54996ee533af59fd8dd823e98680
Tristan Long <longtr@oregonstate.edu>
2023-09-04 15:16:36 -0400

    Pointer practice 2

--- Part 1/section2.cpp
+++ Part 1/section2.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 int main(){
 
-    int a = 10;
     int *p;
-    p=&a;
+    p=(int *)malloc(5*sizeof(int));
+    //p=new int[5] (equivalent to line 9)
 
-    cout<<a;
-    printf(" using pointer %d", *p);
+    p[0]=10; p[1]=15; p[2]=14; p[3]=21; p[4]=31;
+
+    for(int i=0;i<5;i++)
+    cout<<p[i]<<endl;
 
     return 0;
 }
